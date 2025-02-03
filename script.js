4b4c8b5cac01f543352e9dbacd549dba0b8030e9
Grace <gracek2705@tuckahoeschools.org>
2025-02-03 13:31:22 +0000

    commit

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let questionIndex = 0;
 
 
 const question = document.querySelector('.question-container');
+const button = document.querySelector('.submit-button');
+const input = document.querySelector('#input-field');
+let scoreContainer = document.querySelector('.score-container');
+
+question.textContent = questionList
